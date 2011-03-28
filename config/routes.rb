bc869f4530b18e34ecb0ab0e8c63c34d58f21f1f
Jason Younker <jason@28dev.com>
2011-03-28 00:27:04 -0700

    Contact page done

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Yakimapress::Application.routes.draw do
 
   devise_for :users
 
+  match '/about', :to => 'home#about', :as => :about
+  match '/q', :to => 'home#faq', :as => :faq
+
   root :to => 'home#home', :as => :home
   root :to => 'home#home', :as => :root
-  root :to => 'home#about', :as => :about
-  root :to => 'home#faq', :as => :faq
 
   # Hmmmm, if we have this, we lose the contact paths given to us by the contact engine
   # match '*path' => 'products#by_permalink'
