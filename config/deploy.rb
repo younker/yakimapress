930d4529bbcd274bb553c577deb36256e7c6bd4d
Jason Younker <jason@28dev.com>
2011-04-19 10:50:33 -0700

    1) Fixed database names 2) got bundle_install working and called after update_code 3) setup restart to get called after bundle_install

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,8 +10,8 @@ set :webfaction_username, "yakimapress"
 set :webfaction_port, "29299"
 
 set :webfaction_db_type, "mysql"
-set :webfaction_db, "yakimapress_stag"
-set :webfaction_db_username, "yakimapress_stag"
+set :webfaction_db, "yakimapress_prod"
+set :webfaction_db_username, "yakimapress_prod"
 # set :database_yml_template, "database.yml"
 
 set :deploy_to, "/home/#{webfaction_username}/webapps-releases/#{application}"
@@ -21,8 +21,6 @@ set :scm_user, "younker"
 set :scm_verbose, true
 set :repository, "git@github.com:younker/yakimapress.git"
  
-
-
 set :user, "#{webfaction_username}"
 set :use_sudo, false 
  
@@ -32,15 +30,33 @@ role :app, domain
 role :web, domain
 role :db,  domain, :primary => true
  
+default_environment['PATH']='/home/yakimapress/webapps/git/bin:/home/yakimapress/webapps/yakimapress/bin:/home/yakimapress/bin:/usr/local/bin:/usr/local/mysql/bin:/opt/local/apache2/bin:/opt/local/bin:/Users/younker/.rvm/gems/ruby-1.8.7-p302/bin:/usr/kerberos/bin:/bin:/usr/bin'
+default_environment['GEM_HOME']='/home/yakimapress/webapps/yakimapress/gems'
 
 # Basic Capistrano Deployment Steps:
 #   deploy:update_code
 #   deploy:symlink
 #   deploy:restart
 namespace :deploy do
-  ##
-  ## Overriden methods
-  ##
+  desc "Symlink all the /shared assets to their new version-specific path"
+  task :symlink do
+    # release_path = /home/yakimapress/webapps-releases/yakimapress/releases/20110407164415
+    # deploy_to    = /home/yakimapress/webapps-releases/yakimapress
+    run "ln -nfs #{release_path} #{deploy_to}/current"
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/contact.yml #{release_path}/config/contact.yml"
+    run "ln -nfs #{shared_path}/config/ecommerce.yml #{release_path}/config/ecommerce.yml"
+    run "ln -nfs #{shared_path}/assets #{release_path}/public/"
+    run "ln -nfs #{shared_path}/certs #{release_path}"
+  end
+
+  desc "Redefine deploy:restart"
+  task :restart, :roles => :app do
+    run "/home/#{webfaction_username}/webapps/#{application}/bin/restart"
+  end
+  # TODO younker [2011-04-19 10:05]
+  # For some reasons, deploy:restart is NOT firing after deploy:symlink. Figure out why, but until then...
+  after "deploy:bundle_install", "deploy:restart"
 
   desc "Redefine deploy:start"
   task :start, :roles => :app do
@@ -52,51 +68,15 @@ namespace :deploy do
     run "/home/#{webfaction_username}/webapps/#{application}/bin/stop"
   end
 
-  desc "Redefine deploy:restart"
-  task :restart, :roles => :app do
-    run "/home/#{webfaction_username}/webapps/#{application}/bin/restart"
-  end
 
   ##
-  ## Custom Tasks wired into various stages of the basic deployment process
+  ## Custom Tasks
   ##
-  desc "Symlink all the /shared assets to their new version-specific path"
-  task :symlink do
-
-    # run "ln -nfs #{release_path}/public /home/#{webfaction_username}/webapps/#{application}/"
-
-    # release_path = /home/yakimapress/webapps-releases/yakimapress/releases/20110407164415
-    # deploy_to    = /home/yakimapress/webapps-releases/rails
-
-    run "ln -nfs #{release_path} #{deploy_to}/current"
-
-    # ln -nfs /home/younker/webapps-releases/psy/shared/config/database.yml /home/younker/webapps-releases/psy/releases/20100927224516/config/database.yml
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-
-
-
-    # ln -nfs /home/younker/webapps-releases/psy/shared/config/keys.yml /home/younker/webapps-releases/psy/releases/20100927224516/config/keys.yml
-    run "ln -nfs #{shared_path}/config/contact.yml #{release_path}/config/contact.yml"
-    run "ln -nfs #{shared_path}/config/ecommerce.yml #{release_path}/config/ecommerce.yml"
-
-    # ln -nfs /home/younker/webapps-releases/psy/shared/assets /home/younker/webapps-releases/psy/releases/20100927224516/public/
-    run "ln -nfs #{shared_path}/assets #{release_path}/public/"
-
-    # ln -nfs /home/younker/webapps-releases/psy/shared/certs /home/younker/webapps-releases/psy/releases/20100927224516/
-    run "ln -nfs #{shared_path}/certs #{release_path}"
-  end
-  # after "deploy:update_code", "deploy:symlink"
-
-
   desc "bundle install the necessary prerequisites"
   task :bundle_install, :roles => :app do
-    # run "export GEM_HOME=/home/yakimapress/webapps/yakimapress/gems"
     run "cd #{deploy_to}/current/ && /home/yakimapress/webapps/#{application}/bin/bundle install"
   end
-  # after "deploy:update_code", "deploy:bundle_install"
-
-
-
+  after "deploy:symlink", "deploy:bundle_install"
 end
 
 
